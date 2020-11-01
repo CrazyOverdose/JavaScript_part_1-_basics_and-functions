4a5ddb195212a2cf97c50034c55dd7411b78e7bd
CrazyOverdose <47750322+CrazyOverdose@users.noreply.github.com>
2020-11-02 00:18:20 +0300

    Final version

---
+++ Time check for correctness/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Number} hours
+ * @param {Number} minutes
+ * @returns {Boolean}
+ */
+module.exports = function (hours, minutes) {
+
+    if (hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59){
+        return true; 
+    }
+    
+    return false;
+};
